243e6928e9ceba6c42ac18d90903785ff28f1601
Phill Conrad <pconrad@cs.ucsb.edu>
2021-10-20 14:47:24 -0700

    pc - add dining code

---
+++ src/main/java/edu/ucsb/cs156/kitchensink/services/UCSBDiningService.java
@@ -0,0 +1,51 @@
+package edu.ucsb.cs156.spring.backenddemo.services;
+
+import java.util.List;
+import java.util.Map;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.springframework.web.client.RestTemplate;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+
+@Slf4j
+@Service
+public class UCSBDiningService {
+
+    ObjectMapper mapper = new ObjectMapper();
+
+    private final RestTemplate restTemplate;
+
+    public UCSBDiningService(RestTemplateBuilder restTemplateBuilder) {
+        restTemplate = restTemplateBuilder.build();
+    }
+
+    public static final String ENDPOINT = "https://api.ucsb.edu/dining/menu/v1/{date}";
+
+    public String getDiningCommonsJSON(String date) throws HttpClientErrorException {
+        log.info("date={}", date);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(List.of(MediaType.APPLICATION_JSON));
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+      
+        Map<String, String> uriVariables = Map.of("date", date);
+        
+        ResponseEntity<String> re = restTemplate.exchange(ENDPOINT, HttpMethod.GET, entity, String.class,
+                uriVariables);
+        return re.getBody();
+    }
+
+
+}
